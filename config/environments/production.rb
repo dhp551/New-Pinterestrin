c846f070ffcb5630d1e06357cf6e460a73a484f3
dhp551 <dhp551@gmail.com>
2014-08-07 06:14:15 +0530

    Pis Made look good

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,14 @@ Rails.application.configure do
   
   # Fir Heroku
   config.action_mailer.default_url_options = { :host => 'http://kvanaindia.herokuapp.com/' }
+  # paper clip-Amazon
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
   
 end
